fbff898da7f0ac662a349ebec678bae44de3c21e
LeoVerto <LeoVerto@users.noreply.github.com>
2020-04-02 23:48:13 +0200

    Submit results to spacescience

--- doorman.user.js
+++ doorman.user.js
@@ -9,7 +9,8 @@
 // @require      https://github.com/LeoVerto/doorman/raw/master/doorman-lib.js?v=0.9
 // ==/UserScript==
 
-const SUBMIT_URL = "https://librarian.abra.me/submit";
+const SUBMIT_ABRA_URL = "https://librarian.abra.me/submit";
+const SUBMIT_SPACESCIENCE_URL = "https://spacescience.tech/api.php";
 
 function setHint(note, text, overwriteable=false) {
     let hint = note.getElementsByClassName("doorman-hint")[0];
@@ -78,7 +79,7 @@ async function processAnswers(answers) {
 
 async function handleExisting(note, result, source) {
     if (result === "known fake") {
-        setHint(note, result  + " (" + source + ")");
+        setHint(note, result + " (" + source + ")");
         note.setAttribute("style", "background-color: green;")
     } else if (result === "known human") {
         setHint(note, result + " (" + source + ")");
@@ -90,15 +91,16 @@ function submitResults() {
     var notes = document.getElementsByTagName("gremlin-note");
 
     if (notes) {
-        var chosen_text = "";
-        var result = "";
-        var option_texts = [];
+        let chosen_text = "";
+        let result = "";
+        let answers = [];
         for (let note of notes) {
-            var state = note.getAttribute("state");
-            var text_regex = /^\s*(.*)\n/
-            var text = text_regex.exec(note.innerHTML)[1];
+            let state = note.getAttribute("state");
+            let id = note.getAttribute("id");
+            let text_regex = /^\s*(.*)\n/
+            let text = text_regex.exec(note.innerHTML)[1];
 
-            option_texts.push(text);
+            answers.push({id: id, msg: text});
             if (state !== "none") {
                 // Selected answer
                 chosen_text = text;
@@ -106,24 +108,46 @@ function submitResults() {
             }
         }
 
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
+        // Kick off submission in parallel, we don't care about the responses.
+        submitResultsAbra(chosen_text, result, answers.map(x => x.msg));
+        submitResultsSpacescience(chosen_text, result, answers.map(x => [x.id, x.msg]));
+    }
+}
 
-        var raw = JSON.stringify({"chosen_text": chosen_text, option_texts, "result": result});
+async function submitResultsAbra(chosen_text, result, option_texts) {
 
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: raw,
-            redirect: 'follow'
-        };
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-        console.log("Submitting results");
-        fetch(SUBMIT_URL, requestOptions)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
-        }
+    var raw = JSON.stringify({"chosen_text": chosen_text, option_texts, "result": result});
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    console.log("Submitting results");
+    fetch(SUBMIT_ABRA_URL, requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+}
+
+async function submitResultsSpacescience(answer, result, options) {
+    let room =  {"options": options};
+
+    let body = new FormData();
+    body.append("answer", answer);
+    body.append("result", result);
+    body.append("room", JSON.stringify(room));
+    let res = await (await fetch(SUBMIT_SPACESCIENCE_URL, {
+        method: "post",
+        body
+    })).text();
+
+    return JSON.parse(res);
 }
 
 function handleGremlinAction(e) {
@@ -136,6 +160,7 @@ function handleGremlinAction(e) {
         case "link":
             // We don't want to handle this when a new round is started
             if (!window.location.href.startsWith("https://gremlins-api.reddit.com/results")) {
+                // We have to wait a bit for reddit to get the results but after 300ms they redirect us
                 console.log("Submitting results in 250ms");
                 setTimeout(submitResults, 250);
             }
