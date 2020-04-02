a9d56cd5e05c5ceca1c47621f37d79b00235d405
LeoVerto <LeoVerto@users.noreply.github.com>
2020-04-02 03:42:26 +0200

    Submit results to database

--- doorman.user.js
+++ doorman.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name         Doorman - Imposter Helper
 // @namespace    https://leoverto.github.io
-// @version      0.3
+// @version      0.4
 // @author       Leo Verto
 // @include      *
 // @grant        GM.xmlHttpRequest
 // @updateurl    https://github.com/LeoVerto/doorman/raw/master/doorman.user.js
 // ==/UserScript==
+
+const DETECTOR_URL = "https://detector.abra.me/?";
+const CHECK_URL = "https://librarian.abra.me/check";
+const SUBMIT_URL = "https://librarian.abra.me/submit";
 
 function addText(note, text) {
     note.appendChild(document.createTextNode(text));
@@ -52,7 +56,6 @@ function checkExisting(msgs, callback) {
     myHeaders.append("Content-Type", "application/json");
 
     var raw = JSON.stringify({"texts": msgs});
-    console.log(raw);
 
     var requestOptions = {
         method: 'POST',
@@ -61,7 +64,7 @@ function checkExisting(msgs, callback) {
         redirect: 'follow'
     };
 
-    fetch("https://librarian.abra.me/check", requestOptions)
+    fetch(CHECK_URL, requestOptions)
         .then(response => response.json())
         .then(result => callback(result.results))
         .catch(error => console.log('error', error));
@@ -75,18 +78,65 @@ function checkDetector(note, callback) {
         redirect: 'follow'
     };
       
-    fetch("https://detector.abra.me/?" + answer, requestOptions)
+    fetch(DETECTOR_URL + answer, requestOptions)
         .then(response => response.json())
         .then(result => callback(result.fake_probability))
         .catch(error => console.log('error', error));
 }
 
+function submitResults() {
+    var notes = document.getElementsByTagName("gremlin-note");
+
+    if (notes) {
+        var chosen_text = "";
+        var result = "";
+        var option_texts = [];
+        for (let note of notes) {
+            var state = note.getAttribute("state");
+            var text_regex = /^\s*(.*)\n/
+            var text = text_regex.exec(note.innerHTML)[1];
+
+            option_texts.push(text);
+            if (state !== "none") {
+                // Selected answer
+                chosen_text = text;
+                result = state === "correct" ? "WIN" : "LOSE";
+            }
+        }
+
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({"chosen_text": chosen_text, option_texts, "result": result});
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        console.log("Submitting results");
+        fetch(SUBMIT_URL, requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+        }
+}
+
 function handleGremlinAction(e) {
     const type = e.detail.type;
+    console.log(type);
     switch (type) {
         case "begin":
-            console.log("begin!");
-            console.log(getAnswers());
+            console.log("begin");
+            break;
+        case "link":
+            // We don't want to handle this when a new round is started
+            if (!window.location.href.startsWith("https://gremlins-api.reddit.com/results")) {
+                console.log("Submitting results in 250ms");
+                setTimeout(submitResults, 250);
+            }
             break;
         default:
             console.log("default");
@@ -95,11 +145,11 @@ function handleGremlinAction(e) {
 }
 
 function run() {
-    var answers = getAnswers();
-    console.log(answers);
-    processAnswers(answers);
     var app = document.getElementsByTagName("gremlin-app")[0];
     if (app) {
+        var answers = getAnswers();
+        console.log(answers);
+        processAnswers(answers);
         app.addEventListener("gremlin-action", handleGremlinAction);
     }
 }
